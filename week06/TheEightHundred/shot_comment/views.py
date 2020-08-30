129a11fd60e856a3536b2a5c9bae6efb4210df82
chenbw <opt_cbw@hotmail.com>
2020-08-30 21:36:40 +0800

    optimize

--- week06/TheEightHundred/shot_comment/views.py
+++ week06/TheEightHundred/shot_comment/views.py
@@ -9,16 +9,18 @@ from .models import TheEightHundred
 def movie_short(request):
     conditions = Q()
     conditions.add(('star__gte', 3), 'AND')
-    # condtions = {'star__gte': 3}
 
     query = request.environ.get('QUERY_STRING')
     if query:
-        temp = Q()
         q = query.split('=')[1]
+
+        temp = Q()
         temp.connector = 'OR'
+        temp.children.append(('id__icontains', q))
         temp.children.append(('comment__icontains', q))
         temp.children.append(('star__icontains', q))
         temp.children.append(('time__icontains', q))
+
         conditions.add(temp, 'AND')
     shorts_gte_3 = TheEightHundred.objects.filter(conditions)
 
