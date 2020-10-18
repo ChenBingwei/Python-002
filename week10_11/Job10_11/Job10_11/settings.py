fb2f0768ea312a3d60415e489a5e0e1265cc4735
chenbw <opt_cbw@hotmail.com>
2020-10-18 17:21:42 +0800

    optimize

--- week10_11/Job10_11/Job10_11/settings.py
+++ week10_11/Job10_11/Job10_11/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'chenbw',
         'USER': 'root',
-        'PASSWORD': 'cc940211',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
